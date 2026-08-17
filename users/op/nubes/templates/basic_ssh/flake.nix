e45712e104e6aa130f85e064a3195bb192fbe219
op <op@qixos.org>
2026-08-17 10:14:10 +0000

    Add a template with a split ssh client

---
+++ users/op/nubes/templates/basic_ssh/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = ''
+    basic nube template
+    '';
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    opQixCommunity = {
+      url = "path:../../../";
+    };
+
+    qixCore = {
+      url = "git+https://github.com/originalposter/qixos?ref=unstable";
+    };
+  };
+
+  outputs = { self, nixpkgs, opQixCommunity, qixCore, ... }:
+  {
+    qixosTemplateConfigurations.default = qixCore.lib.mkNubeTemplate { inherit nixpkgs; } {
+      modules = [ 
+        opQixCommunity.nixosModules.modules.qubes-split-ssh-client
+        ({ lib, ... }: { qubesSplitSsh = { enable = true; vaultName = lib.mkDefault "split-ssh-nube"; }; })
+        opQixCommunity.nixosModules.modules.blueprints.basic-template
+      ];
+    };
+
+    nixosConfigurations.default = self.qixosTemplateConfigurations.default.nixosConfigurations.default;
+
+  };
+}
