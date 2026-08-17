a1572cfdf82bd3fbc3b780ff1810687a08d52e7b
op <op@qixos.org>
2026-08-17 10:53:49 +0000

    Templates can reach github ssh

--- users/op/modules/blueprints/basic-template.nix
+++ users/op/modules/blueprints/basic-template.nix
@@ -23,5 +23,20 @@
     EDITOR = "nvim";
   };
 
+  # ssh has no proxy-environment support of any kind, so all_proxy does nothing
+  # for it and a ProxyCommand is the only mechanism. Port 443 rather than 22
+  # because the qubes updates proxy filters 22.
+  programs.ssh.extraConfig = ''
+    Host github.com
+      HostName ssh.github.com
+      Port 443
+      ProxyCommand ${pkgs.netcat-openbsd}/bin/nc -X connect -x 127.0.0.1:8082 %h %p
+
+    Host gitlab.com
+      HostName ssh.gitlab.com
+      Port 443
+      ProxyCommand ${pkgs.netcat-openbsd}/bin/nc -X connect -x 127.0.0.1:8082 %h %p
+  '';
+
   nixpkgs.hostPlatform = "x86_64-linux";
 }
