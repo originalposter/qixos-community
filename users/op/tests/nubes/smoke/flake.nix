3b784174db3eb643a5ae98b4108f8c8952d018ea
op <op@qixos.org>
2026-09-15 13:07:16 +0000

    Update test template flake.lock

--- users/op/tests/nubes/smoke/flake.nix
+++ users/op/tests/nubes/smoke/flake.nix
@@ -15,7 +15,7 @@
 
     # TODO: go back to master when appropriate
     qixCore = {
-      url = "git+https://github.com/originalposter/qixos?ref=unstable";
+      url = "git+https://github.com/originalposter/qixos?ref=switch-oom-handling";
     };
   };
 
